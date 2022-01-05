41ced7382d5019c62ceafb7ab5933ed921b44b5f
priyanka109905 <priyankaborase22@gmail.com>
2022-01-06 00:21:51 +0530

    commit ui

---
+++ src/components/Footer.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+      <div>
+      <ul>
+        <li>
+          <button>Home</button>
+        </li>
+        <li>
+          <button>Careers</button>
+        </li>  
+      </ul>
+      </div>
+      
+      <div>
+      <ul>
+        <li>
+          <button>Privacy Policy</button>
+        </li>
+        <li>
+          <button>Disclaimer</button>
+        </li>
+      </ul>
+      <br></br>
+      <h6> &copy; Copyright Made By: @Priyanka Borase</h6>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
